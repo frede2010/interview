c1c9078ffe571ac3872196283a71c3d45849db97
hezhihan <zhe1@xiaohongshu.com>
2019-03-19 13:21:04 +0800

    单例模式

---
+++ src/main/java/com/furui/designpatterns/singleton/EnumSingleton.java
@@ -0,0 +1,12 @@
+package com.furui.designpatterns.singleton;
+
+/**
+ * @author zhihan.he
+ * @version V1.0
+ * @Title EnumSingleton
+ * @Descripion 枚举
+ * @date 2019/3/19 12:51
+ */
+public enum EnumSingleton {
+     SINGLETON;
+}
--- src/main/java/com/furui/designpatterns/singleton/LasySingleDoubleCheck.java
+++ src/main/java/com/furui/designpatterns/singleton/LasySingleDoubleCheck.java
@@ -9,10 +9,11 @@ public class LasySingleDoubleCheck {
     private LasySingleDoubleCheck(){}
     private static LasySingleDoubleCheck instance;
     public static synchronized LasySingleDoubleCheck getInstance(){
-        if (null == instance) {
+        if (instance == null) {
             synchronized (LasySingleDoubleCheck.class){
-                //非原子操作，仍可能存在极小概率的线程不安全情况
-                instance = new LasySingleDoubleCheck();
+                if(instance == null){
+                    instance = new LasySingleDoubleCheck();
+                }
             }
         }
         return instance;
