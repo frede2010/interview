b83c787c16891a2f4951659fbca39cb751a0116f
furui <furui@lvmama.com>
2019-04-11 11:07:32 +0800

    优化冒泡排序

--- src/main/java/com/furui/algorithm/sort/Sort.java
+++ src/main/java/com/furui/algorithm/sort/Sort.java
@@ -13,7 +13,7 @@ public class Sort {
     public static void dobbleSort(int[] nums){
         int length = nums.length;
         for (int i = 0; i < length - 1; i++){
-            for (int j = 0; j < length - 1; j++) {
+            for (int j = 0; j < length - i - 1; j++) {
                 if (nums[j] > nums [j+1]){
                     nums[j] = nums[j]^nums[j+1];
                     nums[j+1] = nums[j]^nums[j+1];
