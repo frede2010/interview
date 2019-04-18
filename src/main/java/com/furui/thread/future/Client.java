a67d21dd0872a07fdb3a54f2d69c6d4bf529ec60
furui <furui@lvmama.com>
2019-04-18 16:36:00 +0800

    thread future

---
+++ src/main/java/com/furui/thread/future/Client.java
@@ -0,0 +1,39 @@
+package com.furui.thread.future;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @author furui
+ * @date 2019/4/18 0018
+ */
+public class Client {
+    public static void main(String[] args) {
+        Long start = System.currentTimeMillis();
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Task task = new Task();
+        Future<Integer> result = executor.submit(task);
+        executor.shutdown();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+
+        System.out.println("主线程在执行任务");
+
+        try {
+            System.out.println("task运行结果"+result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("所有任务执行完毕");
+        System.out.println(System.currentTimeMillis() - start);
+    }
+}
